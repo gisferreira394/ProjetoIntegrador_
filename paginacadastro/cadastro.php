a1c2ee4de3342c38398ccc43f7fbccb2b3025edd
Gi Silva <112526312+gisferreira394@users.noreply.github.com>
2023-07-11 21:55:32 -0300

    .

--- paginacadastro/cadastro.php
+++ paginacadastro/cadastro.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Papelaria Novo Mundo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
 <link rel="stylesheet" href="style.css">
   </head>
 <body>
@@ -33,10 +32,9 @@
   </div>
   
   <div class=" mt-3 text-center">
-    <button type="submit">Entrar</button>
+    <button class="comic-button">Entrar</button>
   </div>
 </form>
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </body>
 </html>
